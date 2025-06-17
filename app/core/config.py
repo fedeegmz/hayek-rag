494b4811c582a09325a23639fc2e48d7aafe5bcd
Federico Gomez <gomez00federico@gmail.com>
2025-06-17 09:33:07 -0300

    feat: configure dependencies

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    mongodb_uri: str = Field(...)
+    embedding_api_key: str = Field(...)
+
+
+config = Config()
